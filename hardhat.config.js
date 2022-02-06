5952191410ebea944e9cfc6f5fe07a6da6360ade
Malcolm <Bild96@pm.me>
2022-02-06 15:41:12 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
-// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
-// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 80001
     },
-    /*
     mumbai: {
-      // Infura
-      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
-      url: "https://rpc-mumbai.matic.today",
-      accounts: [privateKey]
-    },
-    matic: {
-      // Infura
-      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
-      url: "https://rpc-mainnet.maticvigil.com",
+      url: "https://speedy-nodes-nyc.moralis.io/34fd482dcefadffdc49f8f37/polygon/mumbai",
       accounts: [privateKey]
     }
-    */
   },
   solidity: {
     version: "0.8.4",
@@ -33,5 +21,7 @@ module.exports = {
       }
     }
   }
-};
+}
+
+
 
